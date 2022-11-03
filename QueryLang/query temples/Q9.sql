423faaff5e86165e4178f05c1721617a94f7dc19
submissionsigmod1 <114933801+submissionsigmod1@users.noreply.github.com>
2022-11-03 13:45:04 +0200

    Update Q9.sql

--- QueryLang/query temples/Q9.sql
+++ QueryLang/query temples/Q9.sql
@@ -10,6 +10,6 @@ WHERE C.F1 BETWEEN (SELECT AVG(F1)
 
 /* A query looking for CFs that feature f1 o be between the average and the half of the maximum value of feature f1 in the dataset.
     For example, for the Adult Income dataset and for f1=education the constraint that derive from this query for instance x is
-   (y_ age >  age1) or (y_age =  age2), where y_f is the (unknown) value of feature f of the counterfactual
+   (y_ age >  age1) and (y_age <  age2), where y_f is the (unknown) value of feature f of the counterfactual
     age1 is the average age and age2 is half of the max age
 */
